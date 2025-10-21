d24e4dcb5803c584dc626e88224279b0c45af899
Jordan Sun <zhuoran.sun@wustl.edu>
2025-10-20 23:00:45 -0500

    Fixed dependency issues.

--- .docker/env.dockerfile
+++ .docker/env.dockerfile
@@ -55,6 +55,7 @@ RUN apt-get update && apt-get install -y --no-install-recommends \
         python3 \
         python3-dev \
         python3-venv \
+        python3-pip \
     && rm -rf /var/lib/apt/lists/*
 
 # Other tools
--- campaigns/run-campaign.sh
+++ campaigns/run-campaign.sh
@@ -8,4 +8,8 @@ else
 fi
 VENV_NAME=".venv-$(uname -m)-${MACH}"
 
+if [ ! -d "${VENV_NAME}" ]; then
+  python3 -m venv "${VENV_NAME}"
+fi
+./${VENV_NAME}/bin/pip3 install -r requirements.txt
 ./${VENV_NAME}/bin/python3 campaign_test.py
